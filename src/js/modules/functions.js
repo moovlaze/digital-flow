b35c0953e10c3c256df58692f32e19f910a8cc3a
Max <thewildstar.m@gmail.com>
2023-01-15 19:04:59 +0300

    added header

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,1 +1,14 @@
+export const burger = () => {
+	const burger = document.querySelector(".burger");
+	const menu = document.querySelector(".menu");
 
+	burger.addEventListener("click", () => {
+		menu.classList.add("menu_active");
+	});
+
+	menu.addEventListener("click", (e) => {
+		if (e.target.closest(".menu__close") || e.target.closest(".menu__link")) {
+			menu.classList.remove("menu_active");
+		}
+	});
+};
