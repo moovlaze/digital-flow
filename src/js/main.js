70654a03b9c56afb18dd478f165d987b71867434
Max <thewildstar.m@gmail.com>
2023-01-17 20:45:09 +0300

    added section about

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
 import * as functions from "./modules/functions.js";
+import Swiper, { Navigation, Pagination } from "swiper";
 
 functions.burger();
+
+const swiper = new Swiper(".about__container", {
+	modules: [Navigation, Pagination],
+	direction: "horizontal",
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	breakpoints: {
+		1199.98: {
+			direction: "vertical",
+		},
+	},
+});
