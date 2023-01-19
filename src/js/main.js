01d34e368d4f791efa5b0ed5ba4e9799a9679181
Max <thewildstar.m@gmail.com>
2023-01-19 20:41:13 +0300

    added section portfolio

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,20 @@ const swiper = new Swiper(".about__container", {
 	modules: [Navigation, Pagination],
 	direction: "horizontal",
 	pagination: {
-		el: ".swiper-pagination",
+		el: ".about__pagination",
+		clickable: true,
+	},
+	breakpoints: {
+		1199.98: {
+			direction: "vertical",
+		},
+	},
+});
+const swiper2 = new Swiper(".portfolio__wrapper", {
+	modules: [Navigation, Pagination],
+	direction: "horizontal",
+	pagination: {
+		el: ".portfolio__pagination",
 		clickable: true,
 	},
 	breakpoints: {
